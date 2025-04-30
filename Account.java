b28adaf1bc86081394a3cdff6089f7c807b8c766
Aditya Phalak <136440226+zsjfus@users.noreply.github.com>
2025-04-30 16:48:16 +0530

    Create Account.java

---
+++ Account.java
@@ -0,0 +1,50 @@
+package com.bankapp.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class Account {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long account_number;
+	@Column
+	private String account_holder_name;
+	@Column
+	private Double account_balance;
+	public Account() {
+		
+	}
+	
+	public Account(String account_holder_name, Double account_balance) {
+		super();
+		this.account_holder_name = account_holder_name;
+		this.account_balance = account_balance;
+	}
+
+	public Long getAccount_number() {
+		return account_number;
+	}
+	public void setAccount_number(Long account_number) {
+		this.account_number = account_number;
+	}
+	public String getAccount_holder_name() {
+		return account_holder_name;
+	}
+	public void setAccount_holder_name(String account_holder_name) {
+		this.account_holder_name = account_holder_name;
+	}
+	public Double getAccount_balance() {
+		return account_balance;
+	}
+	public void setAccount_balance(Double account_balance) {
+		this.account_balance = account_balance;
+	}
+	
+
+}
